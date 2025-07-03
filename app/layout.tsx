afabd47d766a40a678588d6a98b9b41677feacdf
thinkpad <bibin@thinkpad>
2025-07-03 22:22:05 +0530

    navbar icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Container from "@/components/global/Container";
+import Provider from "./provider";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
     <body className= {inter.className}>
-      <Navbar/>
-      <Container className="py-20" >{children}</Container>
+      <Provider>
+        <Navbar/>
+        <Container className="py-20" >{children}</Container>
+      </Provider>
     </body>
     </html>
   );
